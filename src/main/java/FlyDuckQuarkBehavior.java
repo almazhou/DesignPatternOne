473a00a36711d68705df48ad4e10c551c4912eee
almazhou <zx_new@126.com>
2013-08-18 17:10:56 +0800

    add shell script

--- src/main/java/FlyDuckQuarkBehavior.java
+++ src/main/java/FlyDuckQuarkBehavior.java
@@ -1,9 +1,10 @@
 
 public class FlyDuckQuarkBehavior implements QuarkBehavior {
     @Override
+
     public String quark() {
-        String message = "I can quark";
-        System.out.println(message);
-        return message;
+        String message1 = "I can quark";
+        System.out.println(message1);
+        return message1;
     }
 }
